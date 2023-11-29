7e3398a0b571e3f09cfc79bb7c7ecf2ca654a516
Belov Dmitrii <belovd90@mail.ru>
2023-11-29 07:14:53 +0300

    feat: testing workspace

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/index.scss';
 import App from './App';
+import Home from './pages/Home/Home';
+import MonthStat from './pages/MonthStat/MonthStat';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,5 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Home />
+    <MonthStat />
   </React.StrictMode>
 );
---
+++ src/pages/MonthStat/MonthStat.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MonthStat.module.scss';
+
+type Props = {}
+
+const MonthStat = (props: Props) => {
+  return (
+    <div className={styles.monthStat}>MonthStat</div>
+  )
+}
+
+export default MonthStat;
