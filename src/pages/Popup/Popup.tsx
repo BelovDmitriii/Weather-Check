75c897ac049e4dc9926c56cd2f14b7177f195bfd
Belov Dmitrii <belovd90@mail.ru>
2023-12-09 08:47:30 +0300

    feat: add current-day-info to popup

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,3 +1,4 @@
+import IconsSvgSelector from '../../assets/images/icons/IconsSvgSelector';
 import CurrentDayItem from '../Home/components/DayItemInfo/CurrentDayItem';
 import { Item } from '../Home/components/DayItemInfo/DayItemInfo';
 import styles from './Popup.module.scss';
@@ -32,14 +33,30 @@ const Popup = (props: Props) => {
     <>
       <div className={styles.blur}></div>
       <div className={styles.popup}>
-      <div className={styles.current_day_info}>
+        <div className={styles.current_day}>
+          <div className={styles.current_day_temp}>
+            -13°
+          </div>
+          <div className={styles.current_day_name}>Среда</div>
+          <div className={styles.current_day_image}>
+            <IconsSvgSelector id='snow' />
+          </div>
+          <div className={styles.current_day_time}>
+            Время: <span>23:45</span>
+          </div>
+          <div className={styles.current_day_city}>
+            Город: <span>Москва</span>
+          </div>
+        </div>
         <div className={styles.current_day_info_items}>
           {items.map((item: Item) => {
             return <CurrentDayItem key={item.icon_id} item={item} />
           })}
         </div>
+        <div className={styles.close_button}>
+          <IconsSvgSelector id="close"/>
+        </div>
       </div>
-    </div>
     </>
   )
 }
